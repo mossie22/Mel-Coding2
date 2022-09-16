25395420fc782472f21394fa9212b75b3222bcc9
mossie22 <asherblackman23@winchendon.org>
2022-09-16 09:47:54 -0400

    commit tax fraud

--- MyClass.java
+++ MyClass.java
@@ -2,5 +2,7 @@ public class MyClass
 {
     public static void main(String[] args){
         System.out.print("Hello, world");
+        System.out.print("Goodnight!");
+        System.out.println("Good morning");
     }
 }
---
+++ hone work/ninesixteen.java
@@ -0,0 +1,13 @@
+public class ninesixteen
+{
+    public static void main(String[] args){
+        System.out.println("My name is Mel!");
+        System.out.println("My favorite hobby is coding!");
+        System.out.println("   ()_____");
+        System.out.println("  //     O)___");
+        System.out.println(" //           )");
+        System.out.println("//      _____/");
+
+
+    }
+}
