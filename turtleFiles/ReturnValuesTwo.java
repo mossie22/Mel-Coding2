7e5d1782aacad891e4219464b3cc2889db58bac1
mossie22 <asherblackman23@winchendon.org>
2022-10-28 09:18:29 -0400

    Create ReturnValuesTwo.java

---
+++ turtleFiles/ReturnValuesTwo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.awt.*;
+import java.lang.Math;
+
+
+public class ReturnValuesTwo {
+    public static void main(String[] args){
+        World world = new World(300,300);
+        Turtle yertle = new Turtle(world);
+        Turtle bertle = new Turtle(world);
+        System.out.println(yertle.getDistance(0,0));
+
+        int bertleXPos = (bertle.getXPos());
+        int bertleYPos = (bertle.getYPos());
+
+        System.out.println(yertle.getDistance(bertleXPos,bertleYPos));
+
+    }
+}
